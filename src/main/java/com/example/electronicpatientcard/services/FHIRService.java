e9ed7532036301ae9ed6accaf7b22ae120152334
Majlo <milosz.karlowicz@gmail.com>
2021-06-12 18:13:43 +0200

    Complete getPatients method

--- src/main/java/com/example/electronicpatientcard/services/FHIRService.java
+++ src/main/java/com/example/electronicpatientcard/services/FHIRService.java
@@ -1,20 +1,52 @@
 package com.example.electronicpatientcard.services;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.MedicationStatement;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class FHIRService {
 
-    private static FhirContext context = FhirContext.forR4();
+    private FhirContext context;
+    private IGenericClient client;
+    private String serverBase;
+
+    public FHIRService() {
+        this.context = FhirContext.forR4();
+        this.serverBase = "http://hapi.fhir.org/baseR4";
+        this.client = context.newRestfulGenericClient(serverBase);
+    }
+
+    public List<Patient> getAllPatients(){
+        Bundle results = client
+                .search()
+                .forResource(Patient.class)
+                .returnBundle(Bundle.class)
+                .execute();
+
+        return  results.getEntry()
+                .stream()
+                .map(bundleEntryComponent -> (Patient)bundleEntryComponent.getResource())
+                .collect(Collectors.toList());
+    }
 
     public Patient getPatientByName(String name){
+        Bundle result = client
+                .search()
+                .forResource(Patient.class)
+                .where(Patient.FAMILY.matches().values(name))
+                .returnBundle(Bundle.class)
+                .execute();
 
-        return null;
+        return result.getEntry().size() > 0 ? (Patient)result.getEntry().get(0).getResource() : null;
     }
 
     public Observation getObservation(Patient patient){
