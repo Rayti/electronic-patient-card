6c2f431608c272f50ae94fecb13cd88f4b2a53c8
Majlo <milosz.karlowicz@gmail.com>
2021-06-12 14:28:11 +0200

    Initial commit

---
+++ src/main/java/com/example/electronicpatientcard/ElectronicPatientCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.electronicpatientcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicPatientCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicPatientCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/electronicpatientcard/ElectronicPatientCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electronicpatientcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicPatientCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
